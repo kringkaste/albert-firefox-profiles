b5f458455df3e39fa0ffd3770a0380c4d1c2bd23
Thomas Schulze <tom@codemonauts.com>
2025-03-28 16:24:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Firefox Profile Launcher
+
+With this extension for [Albert](https://albertlauncher.github.io/), you can start Firefox with one of your available profiles. The extension automatically scans for all available profiles in your home directory.
+
+## Installation
+
+Clone this repo into the Python extension folder of Albert:
+
+```shell
+git clone git@github.com:kringkaste/albert-firefox-profiles.git ~/.local/share/albert/python/plugins/albert-firefox-profiles
+```
+
+Restart Albert and go to the extension tab in the settings and activate the extension.
---
+++ __init__.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+"""
+Launch Firefox with one of the available profiles.
+"""
+
+import os
+import configparser
+from albert import *
+from pathlib import Path
+
+md_iid = '2.0'
+md_version = '1.0'
+md_name = 'Firefox Profile Launcher'
+md_description = 'Launch Firefox with one of the available profiles.'
+md_license = 'MIT'
+md_url = 'https://github.com/kringkaste/albert-firefox-profiles'
+
+
+class Plugin(PluginInstance, GlobalQueryHandler):
+
+    def __init__(self):
+        GlobalQueryHandler.__init__(self,
+                                    id=md_id,
+                                    name=md_name,
+                                    description=md_description,
+                                    defaultTrigger='ff ')
+        PluginInstance.__init__(self, extensions=[self])
+
+    def handleGlobalQuery(self, query):
+        home = Path.home()
+        plugin_dir = Path(__file__).parent
+        rank_items = []
+        if os.path.isfile(home / ".mozilla" / "firefox" / "profiles.ini"):
+            config = configparser.ConfigParser()
+            config.read(home / ".mozilla" / "firefox" / "profiles.ini")
+            for section in config.sections():
+                if section.startswith("Profile"):
+                    rank_items.append(
+                        RankItem(
+                            StandardItem(
+                                id=section,
+                                text=config[section]["Name"],
+                                inputActionText=query.trigger + config[section]["Name"],
+                                iconUrls=["file:" + str(plugin_dir / "firefox.png")],
+                                actions=[
+                                    Action(
+                                        "Open",
+                                        "Open %s" % config[section]["Name"],
+                                        lambda profile=config[section]["Name"]: runDetachedProcess(
+                                            ["firefox", "-P", profile]
+                                        ),
+                                    )
+                                ],
+                            ),
+                            1
+                        )
+                    )
+        else:
+            rank_items.append(
+                RankItem(
+                    StandardItem(
+                        id=md_id,
+                        text="Not found",
+                    ),
+                    1
+                )
+            )
+
+
+        return rank_items
